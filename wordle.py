4c9d7cf59f807b7a300077493bcc0a7d7549d442
Hank Magnuski <hank.magnuski@gmail.com>
2022-05-07 07:55:36 -0700

    Bug fixes and index change
    A few bug fixes and a change to how letters are indexed.

--- wordle.py
+++ wordle.py
@@ -4,7 +4,7 @@ wordle.py -- A Python program to help you solve the wordle game
 
 Sample usage:
 
- wordle.py -n audes -r ion --p1 o --p2 i --n0 n --n3 i --n4 o
+ wordle.py -n audes -r ion --p1 o --p2 i --n1 n --n3 i --n4 o
 
 Complete specification:
 
@@ -17,8 +17,8 @@ Complete specification:
  -f, --file           Input filename
  -h, --help           Help text
  -n, --not            Letters which will discard a word
- --n0, --n1 ...       Disallowed letters in position 0 (first letter)
- --p0, --p1 ...       Required in position 0 (first letter)
+ --n1, --n2 ...       Disallowed letters in position 1 (first letter)
+ --p1, --p2 ...       Required in position 1 (first letter)
  -r, --required       Required letters in the word
  -v, --version        Report program version
 
@@ -26,9 +26,9 @@ The sample given above can be read as follows:
 
 Select a word that does not have the letters "a", "u", "d", "e", "s"
 and must require the letters "i", "o", "n"
-and position 1 must have an "o" <--- columns or positions are numbered left to right starting at 0.
+and position 1 must have an "o" <--- columns or positions are numbered left to right starting at 1.
 and position 2 must have an "i"
-and position 0 must not have an "n"
+and position 1 must not have an "n"
 and position 3 must not have an "i"
 and position 4 must not have an "o"
 and select only quality words <--- the word is found in three different dictionaries (default option -a, --all).
@@ -37,6 +37,11 @@ Strategy:
 First guess: "audio" <--- Has four vowels and one popular consonant
 Second guess: clerk spent, crest, sperm or terms <--- Has only one "e" and four popular consonants
 Second guess: Look at the results of the first line and determine what might be more productive (one of the five words above or some other choice based on the first submission).
+
+Letter frequency in English: e a r i o t n s l c u d p
+Suggests that a good starting word might be "arise" or "raise"
+For 5-letter words the frequency is a, e, s, o, r, i, l, t, n, u, d, c, y
+Suggests that a good starting word might be "arose"
 
 Not included here are the English dictionaries, found on GitHub. Three are used:
 
@@ -61,20 +66,20 @@ import random
 debug = False
 discard = ""
 required = ""
-p0 = ""
 p1 = ""
 p2 = ""
 p3 = ""
 p4 = ""
-n0 = ""
+p5 = ""
 n1 = ""
 n2 = ""
 n3 = ""
 n4 = ""
+n5 = ""
 allwords = False
 
 def Usage():
-    print("Usage: wordle.py -d -h -n -r -v --debug --help --not abc... --n0 abc... --n1 abc...  ... --p0 x --p1 x ... --version")
+    print("Usage: wordle.py -d -h -n -r -v --debug --help --not abc... --n1 abc... --n2 abc...  ... --p1 x --p2 x ... --version")
 
 def load_words(w):
     with open(w) as word_file:
@@ -101,30 +106,24 @@ def second(w):
 
 def main(words, ref, ten, cnt):
 
-
+    awords = []
+    qwords = []
+    
     for w in words:
-
 
         # Words not 5 letters can't be used
         if len(w) != 5 :
             continue
 
         # Check for required letters
-        found = False
-        for r in required:
-            if r in w:
-                found = True
-            else:
-                found = False
+        if len(required) > 0:
+            found = True
+            for r in required:
+                if not r in w:
+                    found = False
             if not found:
-                break
-        if not found:
-            continue
+                continue
 
-        # Find a second guess
-        #if second(w):
-        #    continue
-        
         # Check for discards
         throw_away = False
         for d in discard:
@@ -133,45 +132,47 @@ def main(words, ref, ten, cnt):
         if throw_away:
             continue
         
-        if p0 != "":
-            if w[0] != p0:
-                continue
-            
         if p1 != "":
-            if w[1] != p1:
+            if w[0] != p1:
                 continue
             
         if p2 != "":
-            if w[2] != p2:
+            if w[1] != p2:
                 continue
             
         if p3 != "":
-            if w[3] != p3:
+            if w[2] != p3:
                 continue
             
         if p4 != "":
-            if w[4] != p4:
+            if w[3] != p4:
                 continue
         
+        if p5 != "":
+            if w[4] != p5:
+                continue
+            
         throw_away = False
-        if n0 != "":
-            for d in n0:
-                if w[0] == d:
-                    throw_away = True
+
         if n1 != "":
             for d in n1:
-                if w[1] == d:
+                if w[0] == d:
                     throw_away = True
         if n2 != "":
             for d in n2:
-                if w[2] == d:
+                if w[1] == d:
                     throw_away = True
         if n3 != "":
             for d in n3:
-                if w[3] == d:
+                if w[2] == d:
                     throw_away = True
         if n4 != "":
             for d in n4:
+                if w[3] == d:
+                    throw_away = True
+
+        if n5 != "":
+            for d in n5:
                 if w[4] == d:
                     throw_away = True
 
@@ -180,11 +181,20 @@ def main(words, ref, ten, cnt):
 
         # Discard words that aren't in other dictionaries
 
-        if (w in ref) and (w in ten):
-            print("Quality word --> ", w)
+        if w in ten:
+            qwords.append(w)
         elif allwords:
-            print("Quality less --> ", w)
+            awords.append(w)
+            
+    qwords.sort()
+    for w in qwords:
+        print("Quality word --> ", w)
 
+    awords.sort()
+    if allwords:
+        print("---")
+        for w in awords:
+            print("Quality less --> ", w)
 
 def sorting(words): 
     wordsorted = sorted(words, key=len) 
@@ -197,11 +207,13 @@ if __name__ == '__main__':
     # Get options and call the main program
     #                                                                                            
 
+    print("wordle.py -- For 5-letter words the frequency is a, e, s, o, r, i, l, t, n, u, d, c, y")
+
     filename = 'words_alpha.txt'
     count = 0
 
     try:
-        options, args = getopt.getopt(sys.argv[1:], 'ac:df:g:hn:r:v', ['all', 'count=', 'debug', 'file=', 'game=', 'help', 'not=', 'n0=', 'n1=', 'n2=', 'n3=', 'n4=', 'p0=', 'p1=', 'p2=', 'p3=', 'p4=', 'required=', 'version'])
+        options, args = getopt.getopt(sys.argv[1:], 'ac:df:g:hn:r:v', ['all', 'count=', 'debug', 'file=', 'game=', 'help', 'not=', 'n1=', 'n2=', 'n3=', 'n4=', 'n5=', 'p1=', 'p2=', 'p3=', 'p4=', 'p5=', 'required=', 'version'])
     except getopt.GetoptError:
         Usage()
         sys.exit(2)
@@ -220,8 +232,6 @@ if __name__ == '__main__':
             sys.exit()
         if o in ("-n", "--not"):
             discard = a
-        if o in ("--n0"):
-            n0 = a
         if o in ("--n1"):
             n1 = a
         if o in ("--n2"):
@@ -230,8 +240,8 @@ if __name__ == '__main__':
             n3 = a
         if o in ("--n4"):
             n4 = a
-        if o in ("--p0"):
-            p0 = a
+        if o in ("--n5"):
+            n5 = a
         if o in ("--p1"):
             p1 = a
         if o in ("--p2"):
@@ -240,10 +250,12 @@ if __name__ == '__main__':
             p3 = a
         if o in ("--p4"):
             p4 = a
+        if o in ("--p5"):
+            p5 = a
         if o in ("-r", "--required"):
             required = a
         if o in ("-v", "--version"):
-            print("bee.py Version 1.0")
+            print("wordle.py Version 1.1")
             sys.exit()
         
     english_words = load_words(filename)
@@ -254,3 +266,4 @@ if __name__ == '__main__':
     main(english_words, reference_words, tenthousand_words, count)
 
     sys.exit()
+    
